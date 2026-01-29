a92ee874aca6d3128530d1c95bffe17ea2768a96
IBUMBLEBEE <1095037491@qq.com>
2026-01-29 22:58:13 +0800

    fix: github ci testing

--- Magefile.go
+++ Magefile.go
@@ -4,24 +4,7 @@
 package main
 
 import (
-	// mage:import
-	"log"
-
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
-	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
 )
 
-func BuildAll() error {
-	build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
-		err := sh.Copy("dist/gpx_alert4ml_rsod_server", "rust_dist/gpx_alert4ml_rsod_server")
-		if err != nil {
-			log.Fatalf("Failed to copy gpx_alert4ml_rsod_server: %v", err)
-		}
-		return cfg, nil
-	})
-	mg.Deps(build.BuildAll)
-	return nil
-}
-
-var Default = BuildAll
+var Default = build.BuildAll
